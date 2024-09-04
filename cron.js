5fbbe67c250684544fde173b8c27b511854c8c16
1999010168 <39736840+1999010168@users.noreply.github.com>
2024-09-04 17:26:45 +0800

    fix cron

--- cron.js
+++ cron.js
@@ -18,6 +18,38 @@ function checkIn() {
     }
   }
 }
+
+function earn() {
+  className("android.widget.Image").text("Main").findOne().parent().click();
+  sleep(1000);
+  let reg = /(.*COLLECT YOUR EARNED.*)|(.*EARN \$CORNIO.*)/;
+  do {
+    p = className("android.widget.Button").textMatches(reg).findOne(1000);
+    if (p) {
+      p.click();
+      sleep(1000);
+    }
+  } while (p);
+  p = className("android.widget.Button").text("CLOSE").findOne(3000);
+  if (p) p.click();
+
+  p = className("android.widget.TextView").text("COLLECT").findOne(2000);
+  if (p) {
+    p.click();
+    sleep(1000);
+    p = className("android.widget.Button").text("CLOSE").findOne(3000);
+    if (p) p.click();
+    sleep(1000);
+  }
+
+  p = className("android.widget.TextView").text("OPEN").findOne(1000);
+  if (p) {
+    p.click();
+    sleep(1000);
+    press(250, 626, 20);
+    sleep(1000);
+  }
+}
 function fight() {
   p = className("android.widget.TextView").text("MAIN").findOne(5000);
   if (p) {
@@ -35,14 +67,23 @@ function fight() {
             .findOne(60 * 1000);
           if (p) {
             let img, imgDest, rt;
-            imgDest = images.read("/mnt/windows/Pictures/snow.png");
-            img = captureScreen();
-            rt = images.matchTemplate(img, imgDest, {
-              threshold: 0.7,
-              max: 1,
-            });
+            for (let i = 0; i < 60; i++) {
+              imgDest = images.read("/mnt/windows/Pictures/back.png");
+              img = captureScreen();
+              rt = images.matchTemplate(img, imgDest, {
+                threshold: 0.7,
+                max: 1,
+              });
+              if (rt && rt.matches.length > 0) {
+                break;
+              }
+              sleep(1000);
+            }
             if (rt && rt.matches.length > 0) {
+              log("found back");
               press(rt.matches[0].point.x + 40, rt.matches[0].point.y + 40, 10);
+            } else {
+              log("not found back");
             }
           }
         }
@@ -52,46 +93,35 @@ function fight() {
 }
 
 function start() {
-  // app.openUrl(addr);
+  app.openUrl(addr);
   p = className("android.widget.TextView")
     .text("MAIN")
     .findOne(60 * 1000);
-  if (p) {
-    p.click();
-    sleep(1000);
+  if (!p) {
+    log("no wait");
+    return;
   }
-  p = className("android.widget.Button")
-    .textContains("COLLECT YOUR EARNED")
-    .findOne(1000);
-  if (p) {
-    p.click();
-    p = className("android.widget.Button")
-      .textContains("EARN $CORNIO")
-      .findOne(5000);
-    if (p) p.click();
-    p = className("android.widget.Button").text("CLOSE").findOne(3000);
-    if (p) p.click();
+  try {
+    earn();
+    fight();
+    checkIn();
+  } catch (e) {
+    console.log(e);
   }
-
-  p = className("android.widget.TextView").text("COLLECT").findOne(2000);
-  if (p) {
-    p.click();
-    sleep(1000);
-    p = className("android.widget.Button").text("CLOSE").findOne(3000);
-    if (p) p.click();
-    sleep(1000);
-  }
-  p = className("android.widget.TextView").text("OPEN").findOne(1000);
-  if (p) {
-    p.click();
-    sleep(1000);
-    press(250, 626, 20);
-    sleep(1000);
-  }
-  fight();
-  checkIn();
 }
 module.exports = { start };
 // start();
 // start();
+// if (!requestScreenCapture()) {
+//   toast("请求截图失败");
+//   exit();
+// }
 // fight();
+
+// earn();
+
+// p = className("android.widget.Button").textMatches(reg).findOne(1000);
+// p = className("android.widget.Button")
+//   .text("EARN $CORNIO FARM FOR 3 HOURS")
+//   .findOne(1000);
+// log(p);
